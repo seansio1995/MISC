23c0202c84d89dfeb0795efef554debbac892868
seansio1995 <seansio@163.com>
2017-07-25 04:51:08 -0400

    add python downloader

--- Graph.py
+++ Graph.py
@@ -26,4 +26,51 @@ class Vertex:
 
 
 class Graph:
-    
+    def __init__(self):
+        self.vertexList={}
+        self.numVertices=0
+
+    def addVertex(self,key):
+        self.numVertices=self.numVertices+1
+        newVertex=Vertex(key)
+        self.vertexList[key]=newVertex
+        return newVertex
+
+
+    def getVertex(self,n):
+        if n in self.vertexList:
+            return self.vertexList[n]
+        else:
+            return None
+
+
+    def __contains__(self,n):
+        return n in self.vertexList
+
+
+    def addEdge(self,a,b,cost):
+        if a not in self.vertexList:
+            res=self.addVertex(a)
+        if b not in self.vertexList:
+            res=self.addVertex(b)
+
+        self.vertexList[a].addNeighbor(self.vertexList[b],cost)
+
+
+
+    def getVertices(self):
+        return self.vertexList.keys()
+
+    def __iter__(self):
+        return iter(self.vertexList.values())
+g=Graph()
+for i in range(6):
+    g.addVertex(i)
+g.addEdge(0,1,2)
+
+for vertex in g:
+    print(vertex)
+    print(vertex.getConnections())
+    print("\n")
+
+#print(g.vertexList[0].getWeight(1))
